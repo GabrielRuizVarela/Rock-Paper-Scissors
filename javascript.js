739d41f38f3ea83c30cd973aeb1b714b857cbdf4
GabrielRuizVarela <gabrielruizvarela@gmail.com>
2022-04-12 21:00:47 -0300

    Improved clarity

--- javascript.js
+++ javascript.js
@@ -1,25 +1,25 @@
 function computerPlay(){
     let n = Math.round(Math.random()*2);
     if (n===0) {return 'rock';}
-    else if(n===1){return 'paper'}
-    else if (n===2){return 'scissors'}
+    else if(n===1){return 'paper';}
+    else if (n===2){return 'scissors';}
 }
 
 function firstLetterUpperCase(str){
-    return str.slice(0,1).toUpperCase() + str.slice(1)
+    return str.slice(0,1).toUpperCase() + str.slice(1);
 }
 
 function playRound(playerSelection, computerSelection){
     let player = playerSelection.toLowerCase();
     if(player===computerSelection){
-        return ["Tie, try again!", 0, 0]
+        return ["Tie, try again!", 0, 0];
     }
     else if((player==="rock" && computerSelection==='scissors') || (player==="paper" && computerSelection==="rock") ||
-    player==="scissors" && computerSelection==="paper"){
-        return [`You Win! ${firstLetterUpperCase(player)} beats ${firstLetterUpperCase(computerSelection)}`, 1, 0]
+            player==="scissors" && computerSelection==="paper"){
+        return [`You Win! ${firstLetterUpperCase(player)} beats ${firstLetterUpperCase(computerSelection)}`, 1, 0];
     }
     else {
-        return [`You Lose! ${firstLetterUpperCase(computerSelection)} beats ${firstLetterUpperCase(player)}`, 0 ,1]
+        return [`You Lose! ${firstLetterUpperCase(computerSelection)} beats ${firstLetterUpperCase(player)}`, 0 ,1];
     }
 }
 
@@ -29,7 +29,7 @@ function game(){
         player = prompt(`Best of Five!\nRock, paper, scissors, shoot!`).toLocaleLowerCase();
         if (player!=="rock" && player!=="paper" && player!=="scissors"){
             console.log("Invalid input, please try again")
-            continue
+            continue;
         }
         outcome = playRound(player,computerPlay());
         playerScore += outcome[1];
